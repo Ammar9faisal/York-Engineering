4cd25fab7ef6ebc27018f34e4f8addf236c4d52e
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2023-08-24 17:59:47 +0000

    Added admin permission to use feature

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -11,10 +11,14 @@ module.exports = {
     ]
   },
   run: ({interaction}) => {
-        const kickedUser = interaction.options.getMember('member');
 
-        if (interaction)
-        interaction.options.getMember('member').kick();
-        interaction.reply(`${kickedUser} has been kicked by ${interaction.user}`)
+        if (interaction.member.roles.cache.has('1123816334489301019') === true) {
+          const kickedUser = interaction.options.getMember('member');
+          kickedUser.kick();
+          interaction.reply(`${kickedUser} has been kicked by ${interaction.user}`)}
+        
+        else {
+          interaction.reply('You do not have sufficient permissions for this!')
+        }
     }
 }
